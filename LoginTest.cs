d0f4bd136f3325cb3936c0e130729e2b2e76b3bd
khanhphu <nghkphung95@gmail.com>
2025-02-28 23:20:26 +0700

    rewrite register test, new login and logout test

--- LoginTest.cs
+++ LoginTest.cs
@@ -1,66 +1,285 @@
-﻿//using OpenQA.Selenium;
-//using OpenQA.Selenium.Chrome;
-//using OpenQA.Selenium.Interactions;
-//using Newtonsoft.Json;
-//using OpenQA.Selenium.Support.UI;
-//using SeleniumExtras.WaitHelpers;
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using Newtonsoft.Json;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using OfficeOpenXml;
+using Xunit.Sdk;
+using System.Web.WebPages;
+using System.Timers;
+using static SoftwareQualityAssurance_FoodAndDrink.LoginTest;
+
+namespace SoftwareQualityAssurance_FoodAndDrink
+{
+
+    public class LoginTest
+    {
+        private WebDriver driver;
+        private WebDriverWait wait;
+        private List<string> errorMessage;
+        private int i = 1; //initial row in excel
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Manage().Window.Maximize();
+        }
+        public class User_Login
+        {
+            public string email { get; set; }
+            public string password { get; set; }
+        }
+        #region TC 2.1-> TC 2.7
+        [TestCaseSource(nameof(readFileFromExcel))]
+        public void TestLogin(User_Login user_Login)
+        {
+            driver.Navigate().GoToUrl("https://localhost:44379/");
+            string initialUrl = driver.Url;
+            errorMessage = new List<string>();
+            //drop down
+            IWebElement dropdown = driver.FindElement(By.ClassName("dropdown"));
+            Actions action = new Actions(driver);
+            action.MoveToElement(dropdown).Perform();
+            IWebElement loginButton = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='/account/login']")));
+            loginButton.Click();
+            Thread.Sleep(2000);
+            IWebElement email = driver.FindElement(By.XPath("//*[@id='UserName']"));
+            email.SendKeys(user_Login.email);
+            Thread.Sleep(2000);
+            IWebElement password = driver.FindElement(By.XPath("//*[@id='Password']"));
+            password.SendKeys(user_Login.password);
+            Thread.Sleep(2000);
+            IWebElement btn_Login = wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[4]/div/input"))));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", btn_Login);
+            btn_Login.Click();
+            //TC 2.1: Login thanh cong
+            if (driver.Url != initialUrl && driver.Url.Equals("https://localhost:44379/trang-chu"))
+            {
+                IWebElement btn_welcome = driver.FindElement(By.XPath($"//button[contains(text(),'Chào {user_Login.email}')]"));
+                errorMessage.Add("Login success!");
+            }
+            else
+            {
+                IReadOnlyCollection<IWebElement> elements = driver.FindElements(By.XPath("//div[contains(@class,'text-danger')]//li"));
+                if (elements.Count > 0)
+                {
+                    IWebElement element = elements.First();
+                    errorMessage.Add((string)((IJavaScriptExecutor)driver).ExecuteScript("return arguments[0].textContent;", element));
+                }
+                else
+                {
+                    if (email.Text.IsEmpty() && password.Text.IsEmpty())
+                    {
+                        IWebElement txtErrorEmail = driver.FindElement(By.XPath("//span[@data-valmsg-for='UserName']"));
+                        IWebElement txtErrorPass = driver.FindElement(By.XPath("//span[@data-valmsg-for='Password']"));
+                        errorMessage.Add(txtErrorEmail.Text);
+                        errorMessage.Add(txtErrorPass.Text);
+
+                    }
+                    else if (email.Text.IsEmpty())
+                    {
+                        IWebElement txtErrorEmail = driver.FindElement(By.XPath("//span[@data-valmsg-for='UserName']"));
+                        errorMessage.Add(txtErrorEmail.Text);
+
+                    }
+                    else if (password.Text.IsEmpty())
+                    {
+
+                        IWebElement txtErrorPass = driver.FindElement(By.XPath("//span[@data-valmsg-for='Password']"));
+                        errorMessage.Add(txtErrorPass.Text);
 
 
-//namespace SoftwareQualityAssurance_FoodAndDrink
-//{
-//    public class LoginTest
-//    {
-//        private IWebDriver driver;
+                    }
 
-//        public class User
-//        {
-//            public string UserName { get; set; }
-//            public string Password { get; set; }
-//        }
+                }
+            }
+            i++;
+            string allError = "";
+            for (int j = 0; j < errorMessage.Count; j++)
+            {
+                TestContext.WriteLine(errorMessage[j].ToString());
+                allError += errorMessage[j].ToString();
+            }
+            writeResultForExcel(i, allError);
+        }
+        #endregion
 
-//        [SetUp]
-//        public void Setup()
-//        {
-//            driver = new ChromeDriver();
-//        }
 
-//        [Test]
-//        [TestCaseSource(nameof(GetUsers))]
-//        public void TestLogin(User user)
-//        {
-//            driver.Navigate().GoToUrl("https://localhost:44379/");
+        #region Handle excel file
+        private void writeResultForExcel(int i,string e)
+        {
+        
+            ExcelPackage.LicenseContext = LicenseContext.Commercial;
+            string path = @"C:\Users\Phung\source\repos\SoftwareQualityAssurance_FoodAndDrink\TestData\DataEntry.xlsx";
+            using (var package = new ExcelPackage(path))
+            {
+                string sheetName = "Login Data";
+                ExcelWorksheet sheet = package.Workbook.Worksheets[sheetName];
+
+                sheet.Cells[i, 11].Value = e;
+                package.Save();
+
+            }
+        }
+        public static IEnumerable<object[]> readFileFromExcel()
+        {
+            var login_Users = new List<object[]>();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            string path = @"C:\Users\Phung\source\repos\SoftwareQualityAssurance_FoodAndDrink\TestData\DataEntry.xlsx";
+            using (var package = new ExcelPackage(new FileInfo(path)))
+            {
+                string sheetName = "Login Data";
+                ExcelWorksheet sheet = package.Workbook.Worksheets[sheetName];
+
+                for (int i = 2; i <= 8; i++)
+                {
+                    string data = sheet.Cells[i, 8].Text;
+                    string[] lines = data.Split('\n');
+
+
+                    var user = new User_Login
+                    {
+                        email = lines[0].Split('"')[1],
+                        password = lines[1].Split('"')[1]
+
+                    };
+                    login_Users.Add(new object[] { user });
+
+                }
+            }
+            return login_Users;
+
+        }
+        #endregion
+
+
+        #region TC 2.8: Remember me 
+        [Test]
+        public void TestRememberMe()
+        {
+            User_Login user_Login = new User_Login()
+            {
+                email = "example@gmail.com",
+                password = "strongPassword*12345"
+            };
+            driver.Navigate().GoToUrl("https://localhost:44379/");
+            IWebElement dropdown = driver.FindElement(By.ClassName("dropdown"));
+            Actions action = new Actions(driver);
+            action.MoveToElement(dropdown).Perform();
+            IWebElement loginButton = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='/account/login']")));
+            loginButton.Click();
+            IWebElement email = driver.FindElement(By.XPath("//*[@id='UserName']"));
+            email.SendKeys(user_Login.email);
+            Thread.Sleep(2000);
+            IWebElement password = driver.FindElement(By.XPath("//*[@id='Password']"));
+            password.SendKeys(user_Login.password);
+            Thread.Sleep(2000);
+            IWebElement checkRemember = wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.XPath("//*[@id='RememberMe']"))));
+            checkRemember.Click();
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", checkRemember);
+            IWebElement btn_Login = wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[4]/div/input"))));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", btn_Login);
+            btn_Login.Click();
+          
+             driver.Quit();
            
+            driver=new ChromeDriver();
+            driver.Navigate().GoToUrl("https://localhost:44379/trang-chu");
+            wait=new WebDriverWait(driver,TimeSpan.FromSeconds(10));
+            try
+            {
+                var check = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($"//button[contains(text(),'Chào {user_Login.email}')]")));
+                Assert.That(check.Displayed, Is.True);
+            }
+            catch {
+                driver.Close();
+                string message = "Can not remember account";
+                TestContext.WriteLine(message);
+                writeResultForExcel(9, message);
+            }
 
-//            // Di chuyển đến dropdown và nhấn nút đăng nhập
-//            IWebElement dropdown = driver.FindElement(By.ClassName("dropdown"));
-//            Actions actions = new Actions(driver);
-//            actions.MoveToElement(dropdown).Perform();
-//            driver.FindElement(By.CssSelector("a[href='/account/login']")).Click();
-           
 
-//            // Điền thông tin đăng nhập
-//            driver.FindElement(By.Id("UserName")).SendKeys(user.UserName);
-//            driver.FindElement(By.Id("Password")).SendKeys(user.Password);
-//            driver.FindElement(By.XPath("//input[@type='submit' and @value='Đăng nhập']")).Submit();
-           
+        }
+        #endregion
 
-//        }
 
-//        public static IEnumerable<User> GetUsers()
-//        {
-//            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestData", "registerInfor.json"); // Đường dẫn tệp JSON
-//            string json = File.ReadAllText(filePath);
-//            return JsonConvert.DeserializeObject<List<User>>(json);
-//        }
+        #region TC 2.9: Two accounts
+        [Test]
+        public void LoginWith2Accounts()
+        {
+            User_Login user1 = new User_Login
+            {
+                email = "example@gmail.com",
+                password = "strongPassword*12345"
+            };
+            User_Login user2 = new User_Login
+            {
+                email = "newUser@gmail.com",
+                password = "strongPassword*12345"
+            };
+            driver.Navigate().GoToUrl("https://localhost:44379/");
+            //thuc hien dang nhap voi trang dau tien
+            Thread.Sleep(2000);
+            Login(user1.email, user1.password);
+            // Mở tab mới
+            driver.Manage().Cookies.DeleteAllCookies();
 
-//        [TearDown]
-//        public void TearDown()
-//        {
-//            if (driver != null)
-//            {
-//                driver.Quit();
-//                driver.Dispose();
-//            }
-//        }
-//    }
-//}
+            ((IJavaScriptExecutor)driver).ExecuteScript("window.open('https://localhost:44379/', '_blank');");
+
+            // Lấy danh sách tất cả tab
+            List<string> tabs = new List<string>(driver.WindowHandles);
+
+            driver.SwitchTo().Window(tabs[1]);
+            Login(user2.email, user2.password);
+            driver.SwitchTo().Window(tabs[0]);
+
+            driver.Navigate().Refresh();
+            try
+            {
+                IWebElement btn_User = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($"//button[contains(text(),'Chào {user1.email}')]")));
+                TestContext.WriteLine("user 1 is login");
+
+            }
+            catch
+            {
+                IWebElement btn_User = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($"//button[contains(text(),'Chào {user2.email}')]")));
+                string output = "User 2 is login in both tabs";
+                TestContext.WriteLine(output);
+                writeResultForExcel(10, output);
+
+            };
+        }
+        #endregion
+
+        public void Login(string user_email, string user_pass)
+        {
+            //drop down
+            IWebElement dropdown = driver.FindElement(By.ClassName("dropdown"));
+            Actions action = new Actions(driver);
+            action.MoveToElement(dropdown).Perform();
+            IWebElement loginButton = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='/account/login']")));
+            loginButton.Click();
+            Thread.Sleep(2000);
+            IWebElement email = driver.FindElement(By.XPath("//*[@id='UserName']"));
+            email.SendKeys(user_email);
+            Thread.Sleep(2000);
+            IWebElement password = driver.FindElement(By.XPath("//*[@id='Password']"));
+            password.SendKeys(user_pass);
+            Thread.Sleep(2000);
+            IWebElement btn_Login = wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[4]/div/input"))));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", btn_Login);
+            btn_Login.Click();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver?.Dispose();
+
+
+        }
+    }
+}
--- RegisterTest.cs
+++ RegisterTest.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.BiDi.Modules.Script;
 using OpenQA.Selenium.Chrome;
@@ -12,41 +11,78 @@ using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using OfficeOpenXml;
+using SeleniumExtras.WaitHelpers;
 
 namespace SoftwareQualityAssurance_FoodAndDrink
 {
-    [TestClass]
     public class RegisterTest
     {
         private IWebDriver driver;
+        private int i = 1; //so row trong file excel
+        private string errorMessage;
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+        }
         public class User_Register
         {
             public string email { get; set; }
             public string password { get; set; }
             public string confirmPassword { get; set; }
         }
-        [SetUp]
-        public void SetUp()
+
+        public static IEnumerable<object[]> ReadUsersFromExcel()
         {
-            Console.WriteLine("Starting WebDriver setup...");
-            driver = new ChromeDriver();
-            Console.WriteLine("WebDriver initialized.");
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var users = new List<object[]>();
+            string path = @"C:\Users\Phung\source\repos\SoftwareQualityAssurance_FoodAndDrink\TestData\DataEntry.xlsx";
+            using (var package = new ExcelPackage(new FileInfo(path)))
+            {
+                string sheetName = "Register Data";
+                ExcelWorksheet sheet = package.Workbook.Worksheets[sheetName];
+                for (int row = 2; row <= 13; row++)
+                {
+                    string data = sheet.Cells[row, 8].Text;
+                    string[] lines = data.Split('\n');
+                    if (lines.Length < 3)
+                    {
+                        throw new Exception($"Invalid data format in row {row}. Expected at least 3 lines but found {lines.Length}");
+                    }
+
+                    var user = new User_Register
+                    {
+                        email = lines[0].Split('"')[1],
+                        password = lines[1].Split('"')[1],
+                        confirmPassword = lines[2].Split('"')[1]
+
+
+                    };
+                    users.Add(new object[] { user }); // MSTest requires `object[]`
+
+                }
+                return users;
+
+                
+            }
         }
-        [Test]
-        [TestCaseSource(nameof(GetUserRegister))]
+       
+        [TestCaseSource(nameof(ReadUsersFromExcel))]
         public void TestRegister(User_Register user_Register)
         {
             driver.Navigate().GoToUrl("https://localhost:44379/");
-
+            string initalUrl = driver.Url;
             // Navigate to dropdown and click register
             IWebElement dropdown = driver.FindElement(By.ClassName("dropdown"));
             Actions actions = new Actions(driver);
             actions.MoveToElement(dropdown).Perform();
-            driver.FindElement(By.CssSelector("a[href='/account/Register']")).Click();
-            //wait for 
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-
+            WebDriverWait wait=new WebDriverWait(driver,TimeSpan.FromSeconds(10));
+            IWebElement registerButton = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='/account/Register']")));
+            registerButton.Click();
             //fill textfield
             driver.FindElement(By.Id("Email")).SendKeys(user_Register.email);
             Thread.Sleep(2000);
@@ -58,22 +94,51 @@ namespace SoftwareQualityAssurance_FoodAndDrink
             Thread.Sleep(2000);
 
             driver.FindElement(By.XPath("//input[@type='submit' and @value='Đăng ký']")).Submit();
-            Thread.Sleep(2000);
-            //check if register successfully -> write to userData.json -- update later
-            bool isValid = false;
+            wait.Until(d => d.Url != initalUrl);
+            //register succes -> render login pager
+            if (driver.Url.Equals("https://localhost:44379/trang-chu"))
+            {
+                errorMessage = "Success";
+
+            }
+            else {
+                //doc cac thong bao loi
+                IWebElement element = driver.FindElement(By.XPath("//div[contains(@class,'text-danger')]//li"));
+                IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
+                errorMessage = (string)javaScriptExecutor.ExecuteScript("return arguments[0].textContent;", element);
+            }
+           
+                TestContext.Write(errorMessage);
+                i++;
+                writeExcelFile(i, errorMessage); //ghi vào file các error message
+
 
         }
-        public static IEnumerable<User_Register> GetUserRegister()
+
+
+
+
+        public void writeExcelFile(int i,string errorMessage)
         {
-            //b4 TestData folder not exist in /bin/Debug/net8.0
-            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "TestData", "registerInfor.json"); // Đường dẫn tệp JSON
-            string json = File.ReadAllText(filePath);
-            return JsonConvert.DeserializeObject<List<User_Register>>(json);
+            ExcelPackage.LicenseContext=LicenseContext.Commercial;
+            string path = @"C:\Users\Phung\source\repos\SoftwareQualityAssurance_FoodAndDrink\TestData\DataEntry.xlsx";
+            using (var package = new ExcelPackage(path))
+            {
+                string sheetName = "Register Data";
+                ExcelWorksheet sheet = package.Workbook.Worksheets[sheetName];
 
+                sheet.Cells[i,9].Value = errorMessage; 
+                package.Save();
+
+            }
         }
-        [TearDown]  // Runs after each test
+
+
+
+        [TearDown]
         public void TearDown()
         {
+
             if (driver != null)
             {
                 driver.Quit();
